7f203fc165f6ea8d74da9419de63a7db41a18dd3
yaelito12 <yaelacostatorres@gmail.com>
2025-09-09 07:48:07 -0700

    iniciarServer

--- src/serverr/SerVerr.java
+++ src/serverr/SerVerr.java
@@ -1,18 +1,32 @@
 
 package serverr;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 
 public class SerVerr {
 
   
     public static void main(String[] args) {
       
+      
+        final int PUERTO = 8080;
         
-      System.out.println("Servidor iniciado...");    
-        
-        
-        
-        
+        try (ServerSocket servidor = new ServerSocket(PUERTO)) {
+            System.out.println("Servidor iniciado. Esperando conexiones...");
+
+            while (true) {
+                Socket socket = servidor.accept();
+                System.out.println("Cliente conectado desde: " + socket.getInetAddress());
+
+                socket.close();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error en el servidor: " + e.getMessage());
+        }
     }
-    
-}
+}    
+  
