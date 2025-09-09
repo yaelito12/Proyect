5e43b0b9578414e1799fb42bfd28a809d8eedd75
yaelito12 <yaelacostatorres@gmail.com>
2025-09-09 07:34:16 -0700

    Cliente y servidor inicio

--- src/serverr/SerVerr.java
+++ src/serverr/SerVerr.java
@@ -1,20 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package serverr;
 
-/**
- *
- * @author M7-MQ20-D
- */
+
 public class SerVerr {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+      
+        
+      System.out.println("Servidor iniciado...");    
+        
+        
+        
+        
     }
     
 }
