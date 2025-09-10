31d2f9db2e73ab8b313334821588e3a9219b8b81
yaelito12 <yaelacostatorres@gmail.com>
2025-09-10 09:11:08 -0700

    COMMIT ENTRADA-SALIDA

--- src/serverr/SerVerr.java
+++ src/serverr/SerVerr.java
@@ -1,7 +1,10 @@
 
 package serverr;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -20,13 +23,19 @@ public class SerVerr {
             while (true) {
                 Socket socket = servidor.accept();
                 System.out.println("Cliente conectado desde: " + socket.getInetAddress());
-
+                
+                
+   BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
+                
+                
                 socket.close();
             }
 
         } catch (IOException e) {
             System.out.println("Error en el servidor: " + e.getMessage());
         }
+        
     }
 }    
   
