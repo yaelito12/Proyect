419c0fb3eefcd7cea86ad1c098769d0d549f03a7
yaelito12 <yaelacostatorres@gmail.com>
2025-09-10 08:36:30 -0700

    Cliente conecta server

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,7 +1,13 @@
 package cliente;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Cliente {
-    public static void main(String[] args) {
-        // entrada del programa
+    public static void main(String[] args) throws IOException {
+        
+      Socket socket = new Socket("localhost", 1234);
+        
+         socket.close();
     }
 }
