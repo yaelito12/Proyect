8f3321049ad5576e4b999b1c022ce41eb7ada357
yaelito12 <>
2025-09-10 20:49:00 -0600

    Agregue mensages a los catch

--- src/serverr/SerVerr.java
+++ src/serverr/SerVerr.java
@@ -50,15 +50,20 @@ public class SerVerr {
                 salida.println("¡Hola! Estas conectado al servidor (Hilo: " + Thread.currentThread().getName() + ")");
                 salida.println("Escribe algo:");
                 
+                 String mensaje = entrada.readLine();
+                System.out.println("Cliente " + clienteIP + " escribió: " + mensaje);
+                
+                // Responder al cliente
+                salida.println("Servidor recibió: " + mensaje);
        
             } catch (IOException e) {
-                
+                     System.err.println("Error manejando cliente: " + e.getMessage());
             } finally {
                 try {
                     socket.close();
-                    
+                  System.out.println("Cliente " + socket.getInetAddress() + " desconectado");   
                 } catch (IOException e) {
-                   
+                    System.err.println("Error cerrando socket: " + e.getMessage());
                 }
             }
         }
